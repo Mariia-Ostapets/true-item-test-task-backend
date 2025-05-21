403aab119a72217238e6aa5a8691984317c61824
Mariia_Ostapets <mariyaostapets@gmail.com>
2025-05-21 18:15:38 +0300

    Delete clgs

--- src/server.js
+++ src/server.js
@@ -1,12 +1,9 @@
 import express from 'express';
-// import dotenv from 'dotenv';
 import cors from 'cors';
 import { getEnvVar } from './utils/getEnvVar.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import router from './routers/index.js';
-
-// dotenv.config();
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 
